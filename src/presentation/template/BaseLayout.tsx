2665a27edeca22ed2c40ad59c2b5d14cd0f7c51e
= <Arya@val.id>
2025-05-16 15:21:41 +0700

    feat: BaseLayout

---
+++ src/presentation/template/BaseLayout.tsx
@@ -0,0 +1,17 @@
+import Header from "@/components/header"
+import { Sidebar } from "lucide-react"
+import { Outlet } from "react-router"
+
+export const BaseLayout = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <div className="flex flex-1">
+        <Sidebar />
+        <div className="flex-1 p-6 md:p-6 overflow-auto">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  )
+}
