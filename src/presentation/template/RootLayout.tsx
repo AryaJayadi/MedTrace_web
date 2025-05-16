d33c347636b06fbacc9cb5e7be7b1eb7fbbf67b4
= <Arya@val.id>
2025-05-16 09:54:22 +0700

    feat: root layout

---
+++ src/presentation/template/RootLayout.tsx
@@ -0,0 +1,9 @@
+import { Outlet } from "react-router";
+
+export const RootLayout = () => {
+  return (
+    <div className="w-screen h-screen overflow-auto scrollbar-hide">
+      <Outlet />
+    </div>
+  )
+}
