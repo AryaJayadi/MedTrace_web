dbfc82c87fe25c5cc6fb13a6f36c053672e503f9
= <Arya@val.id>
2025-05-26 07:21:58 +0700

    feat: auth models

---
+++ src/domain/model/auth/AuthRequest.ts
@@ -0,0 +1,4 @@
+export interface AuthRequest {
+  organization: string,
+  password: string
+}
---
+++ src/domain/model/auth/LoginResponse.ts
@@ -0,0 +1,5 @@
+export interface LoginResponse {
+  Token: string;
+  OrgID: string;
+  Message: string;
+}
