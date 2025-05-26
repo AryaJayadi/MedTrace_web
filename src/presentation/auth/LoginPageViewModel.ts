fcb223affd8519c06b52e8f113dfb98b4d28de7f
= <Arya@val.id>
2025-05-26 11:30:11 +0700

    feat: better Login and AuthContext

--- src/presentation/auth/LoginPageViewModel.ts
+++ src/presentation/auth/LoginPageViewModel.ts
@@ -1,7 +1,4 @@
 import { ROUTES } from "@/core/Routes";
-import { AuthApiDataSource } from "@/data/datasource/api/AuthApiDataSource";
-import { AuthRepositoryDataSource } from "@/data/repository/AuthRepositoryDataSource";
-import { Login as LoginUseCase } from "@/domain/usecase/auth/Login";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router";
@@ -9,7 +6,7 @@ import { z } from "zod";
 import { toast } from "sonner"
 import { useAuth } from "@/presentation/context/AuthContext";
 import { LoginRequest } from "@/domain/model/auth/LoginRequest";
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 
 const formSchema = z.object({
   organization: z.string().min(4, "Organization is required"),
@@ -21,8 +18,8 @@ type LoginFormValues = z.infer<typeof formSchema>;
 export default function LoginPageViewModel() {
   const navigate = useNavigate();
   const {
-    isAuthenticated,
-    login
+    handleLogin,
+    isAuthenticated
   } = useAuth();
   const [isLoading, setIsLoading] = useState(false);
   const [apiError, setApiError] = useState<string | null>(null);
@@ -35,16 +32,9 @@ export default function LoginPageViewModel() {
     },
   });
 
-  const authDataSource = useMemo(() => new AuthApiDataSource(), []);
-  const authRepository = useMemo(() => new AuthRepositoryDataSource(authDataSource), [authDataSource]);
-  const loginUseCase = useMemo(() => new LoginUseCase(authRepository), [authRepository]);
-  const submitLogin = useCallback(async (request: LoginRequest) => {
-    return await loginUseCase.execute(request);
-  }, [loginUseCase]);
-
   useEffect(() => {
     if (isAuthenticated) {
-      navigate("/app/beranda")
+      navigate(ROUTES.FULL_PATH_APP_BATCH);
     }
   }, [isAuthenticated, navigate])
 
@@ -57,24 +47,19 @@ export default function LoginPageViewModel() {
       password: values.password,
     };
 
-    try {
-      const result = await submitLogin(request);
-      if (result.success && result.value) {
-        login(result.value.Token, result.value.OrgID);
-        toast.success("Login Successful", {
-          description: `Welcome, ${result.value.OrgID}!`,
-        });
-        navigate(ROUTES.FULL_PATH_APP_BATCH);
-      } else {
-        const errorMessage = result.error?.message || "Login failed. Please check your credentials.";
-        setApiError(errorMessage);
-        toast.error("Login Failed", { description: errorMessage });
-      }
-    } catch (error: any) {
-      const errorMessage = error.message || "An unexpected error occurred during login.";
+    const res = await handleLogin(request);
+    if (res && res.value && res.success) {
+      toast.success("Login Successful", {
+        description: `Welcome, ${res.value.OrgID}!`,
+      });
+      setApiError(null);
+      navigate(ROUTES.FULL_PATH_APP_BATCH);
+    } else {
+      const errorMessage = res.error?.message || "Login failed. Please check your credentials.";
+      toast.error("Login Failed", { description: errorMessage });
       setApiError(errorMessage);
-      toast.error("Login Error", { description: errorMessage });
     }
+
     setIsLoading(false);
   };
 
--- src/presentation/context/AuthContext.tsx
+++ src/presentation/context/AuthContext.tsx
@@ -1,9 +1,16 @@
-import { createContext, useContext, useState, ReactNode, useEffect } from 'react';
+import { AuthApiDataSource } from '@/data/datasource/api/AuthApiDataSource';
+import { AuthRepositoryDataSource } from '@/data/repository/AuthRepositoryDataSource';
+import { LoginRequest } from '@/domain/model/auth/LoginRequest';
+import { LoginResponse } from '@/domain/model/auth/LoginResponse';
+import { BaseValueResponse } from '@/domain/model/response/BaseValueResponse';
+import { Login } from '@/domain/usecase/auth/Login';
+import { errorValueResponse } from '@/lib/ResponseHelper';
+import { createContext, useContext, useState, ReactNode, useEffect, useCallback, useMemo } from 'react';
 
 interface AuthContextType {
   token: string | null;
   orgId: string | null;
-  login: (token: string, orgId: string) => void;
+  handleLogin: (request: LoginRequest) => Promise<BaseValueResponse<LoginResponse>>;
   logout: () => void;
   isAuthenticated: boolean;
 }
@@ -14,6 +21,15 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [token, setToken] = useState<string | null>(localStorage.getItem('jwtToken'));
   const [orgId, setOrgId] = useState<string | null>(localStorage.getItem('orgId'));
 
+  const authDataSource = useMemo(() => new AuthApiDataSource(), []);
+  const authRepository = useMemo(() => new AuthRepositoryDataSource(authDataSource), [authDataSource]);
+  const loginUseCase = useMemo(() => new Login(authRepository), [authRepository]);
+  const submitLogin = useCallback(async (request: LoginRequest) => {
+    return await loginUseCase.execute(request);
+  }, [loginUseCase]);
+
+  const isAuthenticated = !!token;
+
   useEffect(() => {
     const storedToken = localStorage.getItem('jwtToken');
     const storedOrgId = localStorage.getItem('orgId');
@@ -22,6 +38,21 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
       setOrgId(storedOrgId);
     }
   }, []);
+
+  async function handleLogin(request: LoginRequest): Promise<BaseValueResponse<LoginResponse>> {
+    try {
+      const result = await submitLogin(request);
+      if (result.success && result.value) {
+        login(result.value.Token, result.value.OrgID);
+        return result;
+      } else {
+        return result
+      }
+    } catch (error: any) {
+      const errorMessage = error.message || "An unexpected error occurred during login.";
+      return errorValueResponse<LoginResponse>(500, errorMessage);
+    }
+  }
 
   const login = (newToken: string, newOrgId: string) => {
     localStorage.setItem('jwtToken', newToken);
@@ -37,10 +68,8 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     setOrgId(null);
   };
 
-  const isAuthenticated = !!token;
-
   return (
-    <AuthContext.Provider value={{ token, orgId, login, logout, isAuthenticated }}>
+    <AuthContext.Provider value={{ token, orgId, handleLogin, logout, isAuthenticated }}>
       {children}
     </AuthContext.Provider>
   );
