74b99f3c99c50adc355b22543196589713d8856e
= <Arya@val.id>
2025-05-31 14:00:39 +0700

    feat: working transfer using token

--- src/data/datasource/api/TransferApiDataSource.ts
+++ src/data/datasource/api/TransferApiDataSource.ts
@@ -1,58 +1,46 @@
-import axios from "axios";
 import { TransferDataSource } from "../TransferDataSource";
 import { Transfer } from "@/domain/model/transfer/Transfer";
-import { CreateTransferRequest } from "@/domain/model/dto/CreateTransferRequest";
-import { ProcessTransferRequest } from "@/domain/model/dto/ProcessTransferRequest";
 import { BaseValueResponse } from "@/domain/model/response/BaseValueResponse";
 import { BaseListResponse } from "@/domain/model/response/BaseListResponse";
+import { CreateTransferRequest } from "@/domain/model/transfer/CreateTransferRequest";
+import { ProcessTransferRequest } from "@/domain/model/transfer/ProcessTransferRequest";
+import apiClient from "./axiosInstance";
 
 const BASE_URL = import.meta.env.VITE_BACKEND_URL + "/transfers";
 
 export class TransferApiDataSource implements TransferDataSource {
-  private axiosInstance = axios.create({
-    baseURL: BASE_URL,
-    transformResponse: [function (response) {
-      try {
-        const resp = JSON.parse(response);
-        return resp;
-      } catch (error) {
-        return response; 
-      }
-    }]
-  });
-
   async createTransfer(request: CreateTransferRequest): Promise<BaseValueResponse<Transfer>> {
-    const response = await this.axiosInstance.post<BaseValueResponse<Transfer>>("", request);
+    const response = await apiClient.post<BaseValueResponse<Transfer>>(BASE_URL, request);
     return response.data;
   }
 
   async getMyTransfers(): Promise<BaseListResponse<Transfer>> {
-    const response = await this.axiosInstance.get<BaseListResponse<Transfer>>("/my");
+    const response = await apiClient.get<BaseListResponse<Transfer>>(`${BASE_URL}/my`);
     return response.data;
   }
 
   async getMyOutTransfer(): Promise<BaseListResponse<Transfer>> {
-    const response = await this.axiosInstance.get<BaseListResponse<Transfer>>("/my/outgoing");
+    const response = await apiClient.get<BaseListResponse<Transfer>>(`${BASE_URL}/my/outgoing`);
     return response.data;
   }
 
   async getMyInTransfer(): Promise<BaseListResponse<Transfer>> {
-    const response = await this.axiosInstance.get<BaseListResponse<Transfer>>("/my/incoming");
+    const response = await apiClient.get<BaseListResponse<Transfer>>(`${BASE_URL}/my/incoming`);
     return response.data;
   }
 
   async acceptTransfer(request: ProcessTransferRequest): Promise<BaseValueResponse<Transfer>> {
-    const response = await this.axiosInstance.post<BaseValueResponse<Transfer>>("/accept", request);
+    const response = await apiClient.post<BaseValueResponse<Transfer>>(`${BASE_URL}/accept`, request);
     return response.data;
   }
 
   async rejectTransfer(request: ProcessTransferRequest): Promise<BaseValueResponse<Transfer>> {
-    const response = await this.axiosInstance.post<BaseValueResponse<Transfer>>("/reject", request);
+    const response = await apiClient.post<BaseValueResponse<Transfer>>(`${BASE_URL}/reject`, request);
     return response.data;
   }
 
   async getTransfer(transferID: string): Promise<BaseValueResponse<Transfer>> {
-    const response = await this.axiosInstance.get<BaseValueResponse<Transfer>>(`/${transferID}`);
+    const response = await apiClient.get<BaseValueResponse<Transfer>>(`${BASE_URL}/${transferID}`);
     return response.data;
   }
 } 
