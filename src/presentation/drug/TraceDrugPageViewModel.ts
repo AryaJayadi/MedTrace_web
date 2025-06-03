8e773a1f04a3215773ffe9c77bf481ca12c9cec9
= <Arya@val.id>
2025-06-03 07:30:28 +0700

    feat: history drug on drug trace

--- src/presentation/drug/TraceDrugPageViewModel.ts
+++ src/presentation/drug/TraceDrugPageViewModel.ts
@@ -1,3 +1,22 @@
+import { useApiRequest } from "@/core/hooks/useApiRequest"
+import { DrugApiDataSource } from "@/data/datasource/api/DrugApiDataSource"
+import { DrugRepositoryDataSource } from "@/data/repository/DrugRepositoryDataSource"
+import { GetHistoryDrug } from "@/domain/usecase/history/GetHistoryDrug"
+import { useCallback, useMemo } from "react"
+
 export default function TraceDrugPageViewModel() {
 
+  const drugDataSource = useMemo(() => new DrugApiDataSource(), [])
+  const drugRepository = useMemo(() => new DrugRepositoryDataSource(drugDataSource), [drugDataSource])
+  const getHistoryDrugUseCase = useMemo(() => new GetHistoryDrug(drugRepository), [drugRepository])
+  const getHistoryDrug = useCallback(async (drugID: string) => {
+    return await getHistoryDrugUseCase.execute(drugID)
+  }, [getHistoryDrugUseCase])
+  const {
+
+  } = useApiRequest(getHistoryDrug)
+
+  return {
+
+  }
 }
